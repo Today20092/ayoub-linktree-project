8283b16c943c9f8b6e377c502051264dfd415861
Ayoub Abedrabbo <Ayoub.Abedrabbo@Gmail.com>
2023-12-30 13:25:59 -0500

    added astro icon intergration using npx astro add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,19 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), icon()],
   styles: {
     postcss: {
       plugins: [
         // ... other PostCSS plugins
         // require("prettier-plugin-tailwindcss"),
-      ],
-    },
-  },
+      ]
+    }
+  }
 });
