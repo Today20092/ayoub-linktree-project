3deb4527a83e61c763636b17bfbc9aec58706e73
Ayoub Abedrabbo <Ayoub.Abedrabbo@Gmail.com>
2023-12-30 15:32:31 -0500

    added deploy file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,23 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), icon()],
+  site: "https://today20092.github.io",
+  base: "/my.repo",
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon(),
+  ],
   styles: {
     postcss: {
       plugins: [
         // ... other PostCSS plugins
         // require("prettier-plugin-tailwindcss"),
-      ]
-    }
-  }
+      ],
+    },
+  },
 });
