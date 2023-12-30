84bae88a04a66b7238f60749f6bdf9d35b522913
Ayoub Abedrabbo <Ayoub.Abedrabbo@Gmail.com>
2023-12-30 15:35:00 -0500

    removed base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://today20092.github.io",
-  base: "/my.repo",
+  site: "https://Today20092.github.io",
   integrations: [
     tailwind({
       applyBaseStyles: false,
