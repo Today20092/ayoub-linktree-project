cc2a0e5ba593db5d3ee9c0add3043ba266a7e60d
Ayoub Abedrabbo <Ayoub.Abedrabbo@Gmail.com>
2024-01-03 14:06:04 -0500

    added working keyframe on test.astro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        blob: "blob 6s ease-in-out infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "scale(1)",
+          },
+          "25%": {
+            transform: "scale(1.5)",
+          },
+          "50%": {
+            transform: "scale(1)",
+          },
+          "75%": {
+            transform: "scale(0.8)",
+          },
+          "100%": {
+            transform: "scale(1)",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
