8328840a6e43425ece69e91806a2f206755f0355
Ayoub Abedrabbo <Ayoub.Abedrabbo@Gmail.com>
2026-01-19 16:18:33 -0500

    tailwind update, vcard, and material design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,12 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ayoubabed.xyz",
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    icon(),
-  ],
-  styles: {
-    postcss: {
-      plugins: [
-        // ... other PostCSS plugins
-        // require("prettier-plugin-tailwindcss"),
-      ],
-    },
+  integrations: [icon()],
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
